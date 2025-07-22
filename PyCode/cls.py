6c9f688617fc8ed888992252a9068b8d567009ac
R0Kernel <1947704061@qq.com>
2025-07-22 09:39:27 +0800

    1

--- PyCode/cls.py
+++
@@ -1,9 +0,0 @@
-class File:
-    def __init__(self,filename,filesize):
-        self.name = filename
-        self.size = filesize
-        print(f"{self.name}创建了")
-    
-    def display_info(self):
-        print(f"文件名称{self.name}")
-        print(f"文件大小{self.size}")
